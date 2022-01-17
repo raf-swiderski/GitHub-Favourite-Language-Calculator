20c7283a8ccde9db1b3d43b03f28d72c07f9c1e7
Raf Swiderski <rafswid@icloud.com>
2022-01-17 18:34:37 +0000

    enter custom username

--- static/Interface.js
+++ static/Interface.js
@@ -1,15 +1,14 @@
-const username = "raf-swiderski"
-
 const button = document.querySelector('button');
-
-
 const body = document.body;
 
+
 button.addEventListener('click', () => {
-    displayFavLanguages();
+    const username = document.getElementById('username').value
+    displayFavLanguages(username);
 });
 
-async function displayFavLanguages() {
+
+async function displayFavLanguages(username) {
     
     const data = await getUserRepos(username)
     .then( (repos) => {
@@ -18,36 +17,22 @@ async function displayFavLanguages() {
 }
 
 
-
 function displayLanguages(repos) {
     
     //isolate languages into its own array
-    var languages = isolateLanguages(repos)
+    var languages = isolateLanguages(repos);
 
     // counting the languages
-    var languagesCount = countLanguages(languages)
+    var languagesCount = countLanguages(languages);
 
-    
-    const languageDiv = document.getElementById("languages")
-    const display = []
-
-    languagesCount.forEach(object => {
-        display.push('<p>')
-        display.push(
-            `${object.language} occurs ${object.occurences} times`
-        )
-        display.push('</p>')
-    });
-
-    languageDiv.innerHTML = display.join('');
-    
+    // send to document
+    sendLanguagesToDocument(languagesCount);
 }
 
 function isolateLanguages(repos) {
     var languages = []
     for (let i = 0; i < repos.length; i++) { languages.push(repos[i].language) } 
-    languages = languages.filter(word => word != null) 
-    // filters out null values 
+    languages = languages.filter(word => word != null) // filters out null values 
     return languages
 }
 
@@ -60,11 +45,24 @@ function countLanguages(languages) {
     const uniqLanguages = [...uniqueSet] // => [ HTML, JavaScript, Ruby ]
 
     for (let i = 0; i < uniqLanguages.length; i++) { 
-
         let occurences = languages.filter((language) => {return language === uniqLanguages[i]}).length 
-
         counter.push({ language: uniqLanguages[i], occurences: occurences })
-
     }
     return counter
 }   
+
+
+function sendLanguagesToDocument(languagesCount) {
+    const languageDiv = document.getElementById("languages")
+    const display = []
+
+    languagesCount.forEach(object => {
+        display.push('<p>')
+        display.push(
+            `${object.language} - ${object.occurences} repositories`
+        )
+        display.push('</p>')
+    });
+
+    languageDiv.innerHTML = display.join('');
+}
